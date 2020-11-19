36b4bdae632c517f7670a3e57faea9ececb1de4e
RainCai <RainCai@126.com>
2020-11-19 11:50:52 +0800

    恩

---
+++ app/Admin/Controllers/ProductController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Admin\Repositories\Product;
+use Dcat\Admin\Form;
+use Dcat\Admin\Grid;
+use Dcat\Admin\Show;
+use Dcat\Admin\Controllers\AdminController;
+
+class ProductController extends AdminController
+{
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        return Grid::make(new Product(), function (Grid $grid) {
+            $grid->column('id')->sortable();
+            $grid->column('user_id');
+            $grid->column('name');
+            $grid->column('price');
+            $grid->column('quantity');
+            $grid->column('created_at');
+            $grid->column('updated_at')->sortable();
+        
+            $grid->filter(function (Grid\Filter $filter) {
+                $filter->equal('id');
+        
+            });
+        });
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     *
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        return Show::make($id, new Product(), function (Show $show) {
+            $show->field('id');
+            $show->field('user_id');
+            $show->field('name');
+            $show->field('price');
+            $show->field('quantity');
+            $show->field('created_at');
+            $show->field('updated_at');
+        });
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        return Form::make(new Product(), function (Form $form) {
+            $form->display('id');
+            $form->text('user_id');
+            $form->text('name');
+            $form->text('price');
+            $form->text('quantity');
+        
+            $form->display('created_at');
+            $form->display('updated_at');
+        });
+    }
+}
---
+++ resources/lang/zh-CN/product.php
@@ -0,0 +1,14 @@
+<?php 
+return [
+    'labels' => [
+        'Product' => 'Product',
+    ],
+    'fields' => [
+        'user_id' => '用户id',
+        'name' => '产品名称',
+        'price' => '价格',
+        'quantity' => '数量',
+    ],
+    'options' => [
+    ],
+];
